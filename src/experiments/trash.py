1b2b50bb2b55dc7c5beb180b1e894bd2d3964d8f
MattiaMasci <mascimattia@gmail.com>
2024-08-28 16:16:05 +0200

    Drop files from .gitignore

--- src/experiments/trash.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch import nn
-
-influence_dataloaders = {'train': 3, 'test': 4}
-dataloaders = {'train': 1, 'test': 2, 'influence': influence_dataloaders}
-
-print(dataloaders)
-print(dataloaders['influence'])
